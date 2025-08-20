01594add979f9c479b81cb4ac82ce2d8358c49fa
luiseduardotrujillosalazar6-ctrl <luiseduardotrujillosalazar6@gmail.com>
2025-08-19 20:49:03 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "mi-app-cache-v1";
+const URLS_TO_CACHE = [
+  "index.html",
+  "manifest.json",
+  "icon.png"
+];
+
+// Instalar SW y guardar archivos en caché
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+// Activar SW y limpiar cachés viejos
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Interceptar requests y servir desde cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
